d4c909341dfd348974e6eec3e9831806d25ca412
Kaelesty <74826130+Kaelesty@users.noreply.github.com>
2023-08-27 20:30:34 +0300

    Car Entity

---
+++ Car.py
@@ -0,0 +1,7 @@
+class Car:
+    def __init__(self, brand, vin, engineCap, price, averageSpeed):
+        self.brand = brand
+        self.vin = vin
+        self.engineCap = engineCap
+        self.price = price
+        self.averageSpeed = averageSpeed
