915b67dd31701131c55e474d31b9129c3163adcc
Kaelesty <74826130+Kaelesty@users.noreply.github.com>
2023-08-31 22:09:11 +0300

    Dict conversions

--- entities/Car.py
+++ entities/Car.py
@@ -11,6 +11,24 @@ class Car:
     def __str__(self):
         return f"[{self.vin}]"
 
+    def toDict(self):
+        return {
+            "brand": self.brand,
+            "vin": self.vin,
+            "engineCap": self.engineCap,
+            "price": self.price,
+            "averageSpeed": self.averageSpeed
+        }
+
+
+def carFromDict(raw):
+    return Car(
+        raw["brand"],
+        raw["vin"],
+        raw["engineCap"],
+        raw["price"],
+        raw["averageSpeed"]
+    )
 
 def getRandomCar():
     return Car(
